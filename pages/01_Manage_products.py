791afd17cde53b56b2fe516df468f04db8cfdcdd
czwolf <czwolf@seznam.cz>
2024-08-03 14:10:43 +0200

    add new tab with input for delete unvanted product from scraped data

--- pages/01_Manage_products.py
+++ pages/01_Manage_products.py
@@ -1,11 +1,12 @@
 from pages.modules.url_list import UrlList
 import streamlit as st
+from Scraper import Scraper
 
 st.set_page_config(layout="wide")
 
 UrlList.check_file_exists()
 
-tab1, tab2 = st.tabs(['Spravovat seznam produktů', 'Přidat produkt do seznamu sledovaných'])
+tab1, tab2, tab3 = st.tabs(['Spravovat seznam produktů', 'Přidat produkt do seznamu sledovaných', 'Smazat produkt ze stažených dat'])
 
 with tab1:
     try:
@@ -29,9 +30,9 @@ with tab1:
 
 with tab2:
     st.write("### Přidat produkt")
-    name = st.text_input("Název produktu")
+    name = st.text_input("Název produktu", key="insert_product_name")
     url = st.text_input("URL")
-    save = st.button("Uložit")
+    save = st.button("Uložit", key='save_button')
 
     if save:
         if name and url:
@@ -40,3 +41,15 @@ with tab2:
                 st.success("Uloženo...")
             except OSError:
                 st.error("Produkt se nepovedl zapsat do seznamu.")
+
+with tab3:
+    st.write('### Smazat produkt ze sledovaných dat')
+    delete_product_name = st.text_input('Název produktu', key='delete_product_name')
+    delete = st.button('Smazat produkt ze stažených dat', key='delete_button')
+    if delete_product_name:
+        if delete_product_name:
+            try:
+                Scraper.delete_unwanted_scraped_data(delete_product_name, Scraper.CSVPATH)
+            except OSError:
+                st.error('Produkt se nepovedlo smazat ze seznamu.')
+
